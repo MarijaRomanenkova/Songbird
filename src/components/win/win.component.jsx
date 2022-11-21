937849eed198a1f9f816cdcbc906172c74a4552a
Marija <marija@dailyart.eu>
2022-11-21 13:33:45 +0200

    added confetti

--- src/components/win/win.component.jsx
+++ src/components/win/win.component.jsx
@@ -1,15 +1,25 @@
 import { useContext, Fragment } from "react";
 import { QuestionContext } from '../../contexts/questionContext';
+import React from 'react'
+
+import Confetti from 'react-confetti';
 
 
 const Win = () => {
     const [questionState, dispatch] = useContext(QuestionContext);
     const isWin = questionState.win;
     const score = questionState.score;
+    const width = window.innerWidth;
+    const height = '800px';
 
     return (
         <Fragment>
             <div className={ isWin ? 'win-container' : 'hidden'}>
+                <Confetti
+                width={width}
+                height={height}               
+                
+                />
                 <h1>Поздравляем!</h1> 
                 <h5>
                 Вы прошли викторину и набрали {score} из 30 возможных баллов</h5>
